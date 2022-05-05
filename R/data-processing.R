9226f48e6ac97a987d85549efa31ca50eb28cacf
MTurner0 <margaret.turner1416@gmail.com>
2022-05-05 10:43:57 -0500

    Edges now go between every two consecutive seasons a contestant was on.

--- R/data-processing.R
+++ R/data-processing.R
@@ -22,21 +22,27 @@ is_returner <- sapply(all_castaway_ids,
 returner_ids <- all_castaway_ids[is_returner]
 
 returner_seasons <- lapply(returner_ids, FUN = function(x) {
-  castaways %>% 
+  # Get returner's name
+  name <- castaways %>% 
+    filter(castaway_id == x) %>% 
+    tail(1) %>% 
+    select(full_name) %>% 
+    pull()
+  # Get vector of returner's seasons
+  seasons <- castaways %>% 
     filter(castaway_id == x) %>% 
     select(season) %>% 
     pull() %>% 
     # Get rid of duplicate seasons: i.e. where the "returner" was just on EOE or
     # Redemption Island
     unique()
+  return(c(name, seasons))
 })
-
-names(returner_seasons) <- returner_ids
 
 # Remove "returners" who were just on EOE or RI
 returner_seasons <- returner_seasons[sapply(returner_seasons, 
                                             FUN = function(x) { 
-                                              length(x) > 1 
+                                              length(x) > 2 
                                               })]
 
 # A function that will make an edge between a returner's first and future
@@ -44,20 +50,28 @@ returner_seasons <- returner_seasons[sapply(returner_seasons,
 # TODO: make edges between each pair of seasons a returner was on?
 edge_list_builder <- function(season_vector) {
   n <- length(season_vector)
-  df <- data.frame(source = rep(season_vector[1], n - 1),
-                   target = season_vector[2:n])
+  df <- data.frame(source = season_vector[2:(n-1)],
+                   target = season_vector[3:n],
+                   name = rep(season_vector[1], n-2))
   return(df)
 }
 
 # Count number of returners (edges) between each pair of seasons
 edge_list <- lapply(returner_seasons, FUN = edge_list_builder) %>% 
-  do.call(what = rbind, args = .) %>% 
-  group_by(source, target) %>% summarize(strength = n())
+  do.call(what = rbind, args = .)
+
+edge_list <- edge_list %>% group_by(source, target) %>% 
+  # Create a column indicating the number of contestants within each edge
+  summarize(strength = n()) %>% 
+  ungroup() %>% 
+  right_join(edge_list, by = c("source", "target"))
 
 # Vector of seasons that have had returners
 node_data <- castaways %>% 
   filter(season %in% c(edge_list$source, edge_list$target)) %>% 
   select(season, season_name) %>% 
+  # Remove "Survivor: " from the beginning of every season name
+  mutate(season_name = stringr::str_remove(season_name, "Survivor: ")) %>% 
   unique()
 
 # Create vector indicating season "type"
@@ -73,5 +87,4 @@ network <- igraph::graph_from_data_frame(d = edge_list, directed = FALSE,
 # Transform into JSON
 data_json <- d3r::d3_igraph(network)
 
-write(data_json, "returners.json")
-
+write(data_json, "dist/assets/returners.json")
